35e0a72fe4feaccec9ffdc7624af12d310e43ccf
Fabian Agudelo <fabianagca0710@gmail.com>
2025-06-04 14:10:28 -0500

    new 3

--- README.md
+++
@@ -1,5 +0,0 @@
-# crewritele
-
-El frontend se incia con " npm run dev "
-
-Para iniciar el backend se tienen que levantar los servicios o contenedores en docker, 
--- database/init.sql
+++ database/init.sql
@@ -33,15 +33,7 @@ CREATE TABLE IF NOT EXISTS `comentario` (
   CONSTRAINT `comentario_ibfk_2` FOREIGN KEY (`noticia_id`) REFERENCES `noticia` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=67 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Volcando datos para la tabla noticias.comentario: ~0 rows (aproximadamente)
-INSERT INTO `comentario` (`id`, `contenido`, `autor_id`, `noticia_id`, `fecha_publicacion`) VALUES
-	(60, 'hola, como estan?', 45, 59, '2025-05-08 16:35:02'),
-	(61, 'que hacen?', 45, 59, '2025-05-08 16:35:51'),
-	(62, 'nada y tu?', 52, 59, '2025-05-08 16:45:31'),
-	(63, 'como estas?', 52, 59, '2025-05-08 16:45:53'),
-	(64, 'sadasd', 52, 61, '2025-05-08 16:49:21'),
-	(65, 'sdasdasd', 52, 61, '2025-05-08 16:49:26'),
-	(66, 'sadasdasdasfad', 52, 60, '2025-05-08 16:49:37');
+
 
 -- Volcando estructura para tabla noticias.noticia
 CREATE TABLE IF NOT EXISTS `noticia` (
@@ -56,11 +48,7 @@ CREATE TABLE IF NOT EXISTS `noticia` (
   CONSTRAINT `noticia_ibfk_1` FOREIGN KEY (`autor_id`) REFERENCES `usuario` (`id`) ON DELETE SET NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=62 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Volcando datos para la tabla noticias.noticia: ~1 rows (aproximadamente)
-INSERT INTO `noticia` (`id`, `titulo`, `contenido`, `autor_id`, `fecha_publicacion`, `id_foto_noticia`) VALUES
-	(59, 'la farmacia', '¿Qué es Lorem Ipsum?\r\nLorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.\r\n\r\n¿Por qué lo usamos?\r\nEs un hecho establecido hace demasiado tiempo que un lector se distraerá con el contenido del texto de un sitio mientras que mira su diseño. El punto de usar Lorem Ipsum es que tiene una distribución más o menos normal de las letras, al contrario de usar textos como por ejemplo "Contenido aquí, contenido aquí". Estos textos hacen parecerlo un español que se puede leer. Muchos paquetes de autoedición y editores de páginas web usan el Lorem Ipsum como su texto por defecto, y al hacer una búsqueda de "Lorem Ipsum" va a dar por resultado muchos sitios web que usan este texto si se encuentran en estado de desarrollo. Muchas versiones han evolucionado a través de los años, algunas veces por accidente, otras veces a propósito (por ejemplo insertándole humor y cosas por el estilo).', 45, '2025-05-08 16:26:04', 'NYC9n8H'),
-	(60, 'la farmacia', 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.\r\n', 52, '2025-05-08 16:48:03', 'codeqJP'),
-	(61, 'la patrona', 'sbakjsdnas', 52, '2025-05-08 16:48:34', 'pILvLNu');
+
 
 -- Volcando estructura para tabla noticias.rol
 CREATE TABLE IF NOT EXISTS `rol` (
@@ -93,10 +81,6 @@ CREATE TABLE IF NOT EXISTS `usuario` (
   CONSTRAINT `fk_rol_id` FOREIGN KEY (`rol_id`) REFERENCES `rol` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=53 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Volcando datos para la tabla noticias.usuario: ~2 rows (aproximadamente)
-INSERT INTO `usuario` (`id`, `nombre`, `apellido`, `nombre_usuario`, `correo`, `contrasena`, `foto_perfil`, `rol_id`, `ruta_foto_perfil`) VALUES
-	(45, 'David', 'Trujillo', 'Davidshtp', 'davidalexismedinatrujillo@gmail.com', '$2b$10$/EIQq7YFxQrcSAeR6YxAsul44m8xmljWSBrqBc0OQBzRB8vOEGlYq', NULL, 1, NULL),
-	(52, 'marian', 'medina', 'mona', 'mariana@gmail.com', '$2b$10$TgQwV8V0bZLeBBWXn45PF.DlDWKpjf8q9nadXKpmI.4gZxbtRW6ay', NULL, 2, NULL);
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
